699799f576471068ccdad75db9b35b5c8a9a98ea
Zachary Halvorson <zthalv@bu.edu>
2021-10-26 12:29:25 -0400

    User input selection tests completed

--- test_user_fight_selection.py
+++ test_user_fight_selection.py
@@ -16,9 +16,12 @@ def test_below_range(capfd):
 
     assert err == "Invalid user input\n"
 
+def test_within_range(capfd):
+    p2.input = lambda: "1"
+    p2.fighter_information_request()
+    out, err = capfd.readouterr()
 
-
-
+    assert out != "Invalid user input\n"
 
 # Followed these guides for capturing error printing, simulating user inputs, etc.
 # https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
